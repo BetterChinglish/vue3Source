22bca1311f11baa4cc341e9896d86599566233d0
BetterChinglish <2199840669@qq.com>
2025-02-28 00:12:48 +0800

    打包脚本初始化

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ packages:
 ```
 这样做之后pnpm会将packages文件夹下的所有项目当作一个项目来管理
 
-# 安装vue3
+# 安装依赖
 由于该项目用作项目管理，如果不加-w则不确定是给packages文件夹下哪个项目安装
 ```shell
 pnpm install vue -w
@@ -38,3 +38,31 @@ pnpm install vue -w
 添加-w说明安装到根目录下而非packages文件夹下的项目里
 
 可以将一些公共的依赖放到根目录下，非公共的则放到packages文件夹里对应的项目下
+
+现在安装如下依赖
+```shell
+pnpm install typescript esbuild minimist -D -w
+```
+
+# 创建sripts文件夹
+用于存放打包脚本
+
+先创建一个dev.js
+
+# 创建reactivity文件夹
+在packages下创建reactivity文件夹
+
+# 修改script命令
+```json
+{
+  "scripts": {
+    "dev": "node scripts/dev.js reactivity -f esm"
+  }
+}
+```
+
+这段命令是以node执行scripts文件夹下的dev.js文件
+
+传入reactivity参数表示打包packages/reactivity
+
+-f esm表示以esm格式输出
---
+++ scripts/dev.js
@@ -0,0 +1,31 @@
+import minimist from 'minimist';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+
+// node scripts/dev.js reactivity -f esm
+// 其中 reactivity -f esm === process.argv.slice(2)
+const args = minimist(process.argv.slice(2));
+// console.log(args);  // { _: [ 'reactivity' ], f: 'esm' }
+
+// 打包的项目
+const target = args._[0] || 'reactivity';
+
+// 打包的格式
+const format = args.f || 'iife';
+
+// node 的esm不支持__dirname
+// const entry = resolve(__dirname)
+
+// import.meta.url是当前devjs的绝对路径，但是以file协议开头，使用fileURLToPath转换为绝对路径
+const __filename = fileURLToPath(import.meta.url);
+
+// __dirname是当前文件的目录
+const __dirname = resolve(__filename, '..');
+
+// 使用require语法
+const require = createRequire(import.meta.url);
+
+
+// 我们后续默认每个模块的入口文件都是src/index.ts
+const entry = resolve(__dirname, `../packages/${target}/src/index.ts`);
