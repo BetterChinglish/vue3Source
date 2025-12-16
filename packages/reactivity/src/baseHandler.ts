8545eb50922da4e1c8ccece02bd38bed5a4b2d37
BetterChinglish <2199840669@qq.com>
2025-12-16 18:23:28 +0800

    响应式中使用Reflect及其原因

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -8,9 +8,16 @@ export const mutableHandlers: ProxyHandler<any> = {
       return true;
     }
 
+    // 响应属性与refect映射
+    return Reflect.get(target, key, receiver);
   },
   set(target, key, value, receiver) {
 
+    // 找到属性对应的副作用函数并执行
+
+    Reflect.set(target, key, value, receiver);
     return true;
   }
 }
+// 使用Reflect避免this指向问题，具体分析可见：https://github.com/BetterChinglish/vue3Source/blob/main/read/01%20%E5%93%8D%E5%BA%94%E7%B3%BB%E7%BB%9F/summary.md#5-js%E5%AF%B9%E8%B1%A1%E4%B8%8Eproxy%E7%9A%84%E5%B7%A5%E4%BD%9C%E5%8E%9F%E7%90%86
+
